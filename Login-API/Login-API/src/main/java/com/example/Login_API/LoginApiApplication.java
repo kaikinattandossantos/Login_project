04fb9ec7e252840fb57804224b62ea306eeede9d
kaiki nattan dos santos guedes <kaikinattansg@gmail.com>
2025-01-24 13:10:33 -0300

    spring on

---
+++ Login-API/Login-API/src/main/java/com/example/Login_API/LoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Login_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApiApplication.class, args);
+	}
+
+}
